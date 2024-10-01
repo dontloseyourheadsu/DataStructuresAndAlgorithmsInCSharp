3507606edf09cfcd25e87cb17f628cc49211d254
Jesus Alvarez Sombrerero <93137954+dontloseyourheadsu@users.noreply.github.com>
2024-10-01 12:01:31 -0600

    Added selection sort

---
+++ DataStructuresAndAlgorithmsInCSharp/Algorithms/SelectionSort.cs
@@ -0,0 +1,44 @@
+﻿namespace DataStructuresAndAlgorithmsInCSharp.Algorithms;
+
+public class SelectionSort
+{
+    public static void SortAscending(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i)
+            {
+                (arr[minIndex], arr[i]) = (arr[i], arr[minIndex]);
+            }
+        }
+    }
+
+    public static void SortDescending(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            int maxIndex = i;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] > arr[maxIndex])
+                {
+                    maxIndex = j;
+                }
+            }
+
+            if (maxIndex != i)
+            {
+                (arr[maxIndex], arr[i]) = (arr[i], arr[maxIndex]);
+            }
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithmsInCSharpTests/Algorithms/SelectionSortTests.cs
@@ -0,0 +1,111 @@
+﻿using DataStructuresAndAlgorithmsInCSharp.Algorithms;
+
+namespace DataStructuresAndAlgorithmsInCSharpTests.Algorithms;
+
+public class SelectionSortTests
+{
+    [Fact]
+    public void SortAscending_SortsIntArrayAscending_WhenPassedIntArray()
+    {
+        // Arrange
+        int[] array = [5, 3, 8, 4, 2];
+
+        // Act
+        SelectionSort.SortAscending(array);
+
+        // Assert
+        Assert.Equal([2, 3, 4, 5, 8], array);
+    }
+
+    [Fact]
+    public void SortDescending_SortsIntArrayDescending_WhenPassedIntArray()
+    {
+        // Arrange
+        int[] array = [5, 3, 8, 4, 2];
+
+        // Act
+        SelectionSort.SortDescending(array);
+
+        // Assert
+        Assert.Equal([8, 5, 4, 3, 2], array);
+    }
+
+    [Fact]
+    public void SortAscending_SortsEmptyArray()
+    {
+        // Arrange
+        int[] array = [];
+
+        // Act
+        SelectionSort.SortAscending(array);
+
+        // Assert
+        Assert.Equal([], array);
+    }
+
+    [Fact]
+    public void SortDescending_SortsEmptyArray()
+    {
+        // Arrange
+        int[] array = [];
+
+        // Act
+        SelectionSort.SortDescending(array);
+
+        // Assert
+        Assert.Equal([], array);
+    }
+
+    [Fact]
+    public void SortAscending_SortsArrayWithOneElement()
+    {
+        // Arrange
+        int[] array = [1];
+
+        // Act
+        SelectionSort.SortAscending(array);
+
+        // Assert
+        Assert.Equal([1], array);
+    }
+
+    [Fact]
+    public void SortDescending_SortsArrayWithOneElement()
+    {
+        // Arrange
+        int[] array = [1];
+
+        // Act
+        SelectionSort.SortDescending(array);
+
+        // Assert
+        Assert.Equal([1], array);
+    }
+
+    [Fact]
+    public void SortAscending_SortsArrayWithDuplicateElements()
+    {
+        // Arrange
+        int[] array = [4, 2, 2, 8, 4];
+
+        // Act
+        SelectionSort.SortAscending(array);
+
+        // Assert
+        Assert.Equal([2, 2, 4, 4, 8], array);
+    }
+
+    [Fact]
+    public void SortDescending_SortsArrayWithDuplicateElements()
+    {
+        // Arrange
+        int[] array = [4, 2, 2, 8, 4];
+
+        // Act
+        SelectionSort.SortDescending(array);
+
+        // Assert
+        Assert.Equal([8, 4, 4, 2, 2], array);
+    }
+}
+
